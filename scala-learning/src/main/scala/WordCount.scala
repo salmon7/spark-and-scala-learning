777b917d0c8e1b7c94d8b651eaa75b0aebe33ef7
zql <gzzhangqilong2017@corp.netease.com>
2019-11-01 02:08:34 +0800

    scala demo

---
+++ scala-learning/src/main/scala/WordCount.scala
@@ -0,0 +1,16 @@
+/**
+  * @author zhang 2019-10-31 01:00
+  */
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+  def main(args: Array[String]) {
+    val inputFile =  "/Users/gzzhangqilong2017/Workspace/github/spark-and-scala-learning/scala-learning/src/main/resources/word.txt"
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(inputFile)
+    val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    wordCount.foreach(println)
+  }
+}
